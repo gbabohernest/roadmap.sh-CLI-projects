23cfcbfceb512c1ed28efd793f931e06d50153d3
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-12-07 23:38:04 +0300

    Game constants

---
+++ number guessing game/game_constants.py
@@ -0,0 +1,15 @@
+"""
+Constants for game levels and chances
+"""
+
+GAME_LEVELS = {
+    1: 'Easy',
+    2: 'Medium',
+    3: 'Hard'
+}
+
+CHANCES = {
+    'Easy': 10,
+    'Medium': 5,
+    'Hard': 3
+}
