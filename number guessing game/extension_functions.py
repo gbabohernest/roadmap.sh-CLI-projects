e2eb14f01dc593edbd11b23ab9b092256edceca6
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-12-19 20:25:09 +0300

    loads high scores from a json file if it exits, create a new one if not and return the score dictionary

--- number guessing game/extension_functions.py
+++ number guessing game/extension_functions.py
@@ -2,7 +2,28 @@
 Game extension functions. These functions add extra functionalities to the game.
 """
 
-from game_constants import SCORES_PER_DIFFICULTY, BONUS_MULTIPLIERS
+import json
+from os import path
+from game_constants import SCORES_PER_DIFFICULTY, BONUS_MULTIPLIERS, HIGH_SCORES_FILE
+
+
+def load_high_scores(file_name: str) -> dict:
+    """
+    Load game high score(s) from a Json file or create an emtpy file
+    if file doesn't exist.
+
+    :param file_name: Name of the file
+    :return: A dict containing the high score(s)
+    """
+
+    if not path.exists(file_name):
+        # create the file
+        with open(file_name, 'w', encoding='utf-8') as file_stream_obj:
+            json.dump({}, file_stream_obj)
+
+    # file exist
+    with open(file_name, 'r', encoding='utf-8') as file_obj:
+        return json.load(file_obj)
 
 
 def calculate_user_score(difficulty: str, max_chances: int, attempts_used: int) -> int:
