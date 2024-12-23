818eb292c650b1434987640520d0be62006ab508
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-12-23 18:42:39 +0300

    updates the high scores list with a new or improved score for a player.

--- number guessing game - php/scores_functions.php
+++ number guessing game - php/scores_functions.php
@@ -37,3 +37,25 @@ function saveHighScores(array $scores, string $fileName): void
     file_put_contents($fileName, json_encode($scores, JSON_PRETTY_PRINT));
 
 }
+
+
+/**
+ * Updates the high scores list with a new or improved score for a player.
+ *
+ * @param string $playerName The name of the player.
+ * @param int $score The player's score to update.
+ * @return void
+ */
+function updateHighScores(string $playerName, int $score): void
+{
+    $highScores = loadHighScores(HIGH_SCORES);
+
+    // update score if player exits,  or add new entry.
+    if (!isset($highScores[$playerName])) {
+        $highScores[$playerName] = $score;
+    }
+
+    $highScores[$playerName] = max($highScores[$playerName], $score);
+    saveHighScores($highScores, HIGH_SCORES);
+
+}
