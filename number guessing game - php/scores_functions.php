0045b4a255c6deca11c8b38a81cd66f58de73be0
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-12-23 18:35:36 +0300

    A function that loads high scores from a JSON file and return an asociative array of high scores, with player names as key and scores as value

--- number guessing game - php/scores_functions.php
+++ number guessing game - php/scores_functions.php
@@ -5,9 +5,15 @@
  */
 
 
-function loadHighScores(): array
+require_once 'game_constants.php';
+
+/**
+ * Loads high scores from a JSON file.
+ * @param string $fileName The name of the JSON file containing high scores.
+ * @return array An associative array of high scores, with player names as key and scores as value
+ */
+function loadHighScores(string $fileName): array
 {
-    $fileName = 'high_scores.json';
 
     if (!file_exists($fileName)) {
         file_put_contents($fileName, json_encode([]));
