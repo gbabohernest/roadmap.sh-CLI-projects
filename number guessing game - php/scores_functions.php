9db2889f14f60151a1249dda149b8012f67a9d81
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2024-12-23 18:39:39 +0300

    Saves high scores to a JSON file

--- number guessing game - php/scores_functions.php
+++ number guessing game - php/scores_functions.php
@@ -9,6 +9,7 @@ require_once 'game_constants.php';
 
 /**
  * Loads high scores from a JSON file.
+ *
  * @param string $fileName The name of the JSON file containing high scores.
  * @return array An associative array of high scores, with player names as key and scores as value
  */
@@ -24,3 +25,15 @@ function loadHighScores(string $fileName): array
 }
 
 
+/**
+ * Saves high score to a JSON file.
+ *
+ * @param array $scores An associative array of high scores to save.
+ * @param string $fileName The name of the JSON file to save high scores in.
+ * @return void
+ */
+function saveHighScores(array $scores, string $fileName): void
+{
+    file_put_contents($fileName, json_encode($scores, JSON_PRETTY_PRINT));
+
+}
