61a2d483e34bc82b773cdd73f138354dae841ff7
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-01-08 15:26:32 +0300

    initial commit

---
+++ TMDB CLI Tool/tmdb-app-cli/fetch_movie_details.py
@@ -0,0 +1,15 @@
+"""
+This module contains a function that fetches movie function and display it in the terminal
+"""
+
+
+import json
+from urllib.request import  urlopen, Request
+from urllib.error import HTTPError, URLError
+
+
+def fetch_movie_details():
+    """
+    Fetches movie information using the TMDB API and display in the terminal.
+    """
+    pass
