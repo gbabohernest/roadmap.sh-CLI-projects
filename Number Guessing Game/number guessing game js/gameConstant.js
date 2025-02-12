8d8ee263a22e8821c7820cae8b19cb6b523cdba5
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-02-12 22:58:12 +0300

    game data

---
+++ Number Guessing Game/number guessing game js/gameConstant.js
@@ -0,0 +1,18 @@
+//Game constant
+
+const difficulties = {
+    1: {
+        name: 'Easy',
+        chances: 10,
+    },
+
+    2: {
+        name: 'Medium',
+        chances: 5,
+    },
+
+    3: {
+        name: 'Hard',
+        chances: 3
+    },
+}
