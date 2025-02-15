d3ef8b7c3f69da73c853d3e257034e329e22cedf
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-02-15 17:45:43 +0300

    format gameLevel output

--- Number Guessing Game/number guessing game js/utils.js
+++ Number Guessing Game/number guessing game js/utils.js
@@ -1,27 +1,44 @@
 // This module contains Game helper functions
 
+const gameDifficulties = require('./gameConstant');
+
 /*
 * Prompt user to choose the game level.
 * @return: An integer presenting the game level. [1, 2, 3]
- */
+*/
+
+
 const chooseGameLevel = () => {
     const gameLevel = `
-    Please select the difficulty level:
-    1. Easy (10 chances)
-    2. Medium (5 chances)
-    3. Hard (3 chances)
-    `
-
+       Please select the difficulty level:
+       1. Easy (10 chances)
+       2. Medium (5 chances)
+       3. Hard (3 chances)
+    `;
+    console.log(gameLevel.trimStart());
     const prompt = 'Enter your choice: ';
 
     process.stdout.write(prompt);
 
     process.stdin.on('readable', () => {
-        const input = process.stdin.read();
-        if (input) {
-            process.stdout.write(`thanks for providing your choice ${input}`);
+
+        const input = Number(process.stdin.read());
+
+        if (!isNaN(input)) {
+            // A valid number
+            if (input in gameDifficulties) {
+                console.log(gameDifficulties[input])
+                console.log('exist')
+                // return the game level.
+            } else {
+                console.error('WRONG!!! Please s-elect a valid game level [1, 2, 3]')
+                // prompt user if they want to quit the game or continue playing.
+            }
         }
+        console.log('Invalid input, Please enter a number. [1, 2, 3]');
+        // prompt user if they want to quit the game or continue playing.
     })
 }
+
 
 module.exports = chooseGameLevel;
