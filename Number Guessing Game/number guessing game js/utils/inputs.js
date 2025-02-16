e7be998b668bc3496caf3d1606816e8c2af6ee52
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-02-16 23:33:06 +0300

    A function that gets user input

---
+++ Number Guessing Game/number guessing game js/utils/inputs.js
@@ -0,0 +1,18 @@
+/*
+* This module contains a function that handles user input
+ */
+
+
+const getUserInput = (prompt) => {
+    return new Promise((resolve) => {
+        process.stdout.write(prompt);
+        process.stdin.resume();
+        process.stdin.setEncoding('utf-8');
+
+        process.stdin.once('data', (data) => {
+            resolve(data.trim());
+        })
+    })
+}
+
+module.exports = {getUserInput};
