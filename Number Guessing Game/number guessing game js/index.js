b80c1e162d8cceb80617d2fea94ebb0ad6e87ff6
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-02-16 22:47:52 +0300

    main entry point of the game

--- Number Guessing Game/number guessing game js/index.js
+++ Number Guessing Game/number guessing game js/index.js
@@ -1,7 +1,12 @@
+#!/usr/bin/env node
+
 /*
 * Game Entry point
 * */
 
-const { startGame }  = require('./game')
+const {startGame} = require('./game')
+
+console.log('Welcome to the Number Guessing Game!!');
+
 
 startGame();
