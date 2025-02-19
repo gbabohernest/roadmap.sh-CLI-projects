3755c291685fdd96ebaef7318bc6bdc1124f6b2a
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-02-19 15:33:11 +0300

    update function signatures, use parameters to get data in and out of the functions

--- Number Guessing Game/number guessing game js/utils/userScoreHandler.js
+++ Number Guessing Game/number guessing game js/utils/userScoreHandler.js
@@ -11,23 +11,25 @@ const highScoreFile = path.join(__dirname, "..", HIGH_SCORE_FILE);
 
 /**
  *  Load game high score(s) from a Json file or create an empty file if file doesn't exist.
+ *  @param fileName : String - High score file name.
  *  @return Object - An object containing the file contents.
  */
-const loadHighScore = () => {
+const loadHighScore = (fileName) => {
     //check if the file exits.
-    if (!fs.existsSync(highScoreFile)) {
-        fs.writeFileSync(highScoreFile, JSON.stringify({}, null, 2))
+    if (!fs.existsSync(fileName)) {
+        fs.writeFileSync(fileName, JSON.stringify({}, null, 2))
     }
 
-    return JSON.parse(fs.readFileSync(highScoreFile, 'utf-8'))
+    return JSON.parse(fs.readFileSync(fileName, 'utf-8'))
 }
 
 /**
  * Save high scores back to the JSON file.
  * @param scores : Object  - An object containing the scores.
+ * @param fileName : String -  Name of the file
  */
-const saveHighScores = (scores) => {
-    fs.writeFileSync(highScoreFile, JSON.stringify(scores, null, 2));
+const saveHighScores = (scores, fileName) => {
+    fs.writeFileSync(fileName, JSON.stringify(scores, null, 2));
 }
 
 
@@ -37,11 +39,11 @@ const saveHighScores = (scores) => {
  * @param score : Number - Player's score.
  */
 const updateHighScore = (playerName, score) => {
-    const allHighScores = loadHighScore();
+    const allHighScores = loadHighScore(highScoreFile);
 
     if (!allHighScores[playerName] || score > allHighScores[playerName]) {
         allHighScores[playerName] = score;
-        saveHighScores(allHighScores);
+        saveHighScores(allHighScores, highScoreFile);
         console.log(`New high score for ${playerName} : ${score} `)
     }
 }
