518b9d6db1b7316bc10703a725a2c2387be4fe52
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-02-20 17:33:49 +0300

    Game entry point, get user option and direct the game flow

--- Number Guessing Game/number guessing game js/index.js
+++ Number Guessing Game/number guessing game js/index.js
@@ -4,9 +4,38 @@
 * Game Entry point
 * */
 
-const {startGame} = require('./game')
+const { startGame } = require('./utils/game')
+const { displayLeaderboard } = require('./utils/userScoreHandler');
+const { getUserOption } = require('./utils/getUserOption');
 
-console.log('Welcome to the Number Guessing Game!!');
+async function main () {
+      console.log('Welcome to the Number Guessing Game!!');
+      console.log("Game menu");
+      console.log("1. Play Game\n2. View Leaderboard\n3. Exit");
+
+      let choice;
+
+      do {
+          choice = await getUserOption();
+      } while(!choice);
+
+      if (choice) {
+
+          switch (choice) {
+              case 1:
+                  await startGame();
+                  break;
+              case 2:
+                  displayLeaderboard();
+                  break;
+              case 3:
+                  process.exit(0)
+                  break;
+          }
+      }
+}
+
+main();
 
 
-startGame();
+
