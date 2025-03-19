7e8742ea0963fc8d9ba25177769a5ecb9bb52982
Gbaboh Ernest <gbaboh.ernest@gmail.com>
2025-03-20 00:51:32 +0300

    fetch github activities, return the json data

--- GitHub User Activity/github-activity-js/app.js
+++ GitHub User Activity/github-activity-js/app.js
@@ -10,7 +10,19 @@ console.log(process.argv);
 const fetchGithubUserActivity = async (username) => {
   const apiEndpoint = `https://api.github.com/users/${username}/events`;
 
+  try {
+    const response = await fetch(apiEndpoint, {
+      headers: { "User-Agent": "Node GitHub User Activity" },
+    });
 
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error("User NOT Found, Check user name and try again!!");
+      } else {
+        throw new Error(`Error fetching the resource: ${response.status}`);
+      }
+    }
+    return await response.json();
   } catch (error) {
     console.error(error.message);
   }
